6c0a7aaa21bf78f8696d9fae1c49be2652f7536c
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-23 16:53:47 +0200

    Create CookieHelper.js

---
+++ Static/Scripts/CookieHelper.js
@@ -0,0 +1,27 @@
+function GetCookies() {
+    const CookieTable = document.cookie.split(";")
+    const Cookies = {}
+
+
+    var ArrayLength = CookieTable.length;
+    for (var i = 0; i < ArrayLength; i++) {
+        //console.log(CookieTable[i]);
+        //Do something
+        const Decoded = CookieTable[i].split("=")
+
+        //console.log(Decoded)
+
+        Cookies[Decoded[0]] = Decoded[1] 
+    }
+
+    return Cookies
+}
+
+function GetCookie(Key) {
+    return GetCookies()[Key]
+}
+
+function SetCookie(Key, Value) {
+    document.cookie = Key + "=" + Value
+    return document.cookie
+}
