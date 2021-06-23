84885d5637eafbc04c30d298bc77a06419e41822
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-23 16:53:40 +0200

    discord vars

---
+++ Static/Scripts/DiscordVars.js
@@ -0,0 +1,10 @@
+BASEDISCORD = "https://discord.com/"
+APIURL = "api/"
+APIVERSION = "v8/"
+OAUTH2 = "oauth2/"
+
+USER = "users/@me/"
+
+CLIENT_ID = "769547693876969492"
+
+AUTHURL = BASEDISCORD + APIURL + OAUTH2 + "authorize?client_id=" + CLIENT_ID + "&" + "redirect_uri=https%3A%2F%2Fpanel.codex-bot.ga" + "&" + "response_type=token" + "&" + "scope="+ "email" + "%20" + "guilds" + "%20" + "guilds.join"+ "%20" + "identify"
