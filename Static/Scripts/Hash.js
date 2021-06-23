6a4d0095c2e4a116dc7c4862ad3495beb70c3a6b
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-23 16:54:31 +0200

    hash test

---
+++ Static/Scripts/Hash.js
@@ -0,0 +1,19 @@
+function GetHash() {
+    if (window.location.hash) {
+        const HashTable = window.location.hash.split("#")[1].split("&")
+        const Hash = {}
+        var arrayLength = HashTable.length;
+        for (var i = 0; i < arrayLength; i++) {
+            //console.log(HashTable[i]);
+            //Do something
+            const Hashes = HashTable[i].split("=")
+            Hash[Hashes[0]] = Hashes[1] 
+        }
+
+        console.log(Hash)
+
+        return Hash
+    } else {
+        return null
+    }
+}
