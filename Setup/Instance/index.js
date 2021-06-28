588263a47d978a3a6b1e418c09c2c3d72b4eaa56
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-06-28 21:18:37 +0200

    replit setup page

--- Setup/Instance/index.js
+++ Setup/Instance/index.js
@@ -5,17 +5,39 @@ window.onload = async function() {
     console.log("loaded")
 
     if (Query.check == "") {
+
+        document.getElementById("check").innerHTML = '<img id="image" src="/Static/Images/logo.gif" alt="Loading" height="50px" width="50px">'
+
         const RequestData = await request(
             {
-                method: "GET",
-                baseUrl: "https://replit.com",
-                url: "/data/repls/@" + localStorage.replit_name + "/CODEX-INSTANCE", 
-                headers: {
-                    'Content-Type': 'text/plain'
-                },
+                method: "POST",
+                baseUrl: "https://Fetch-Proxy.scriptitwithcod.repl.co",
+                url: "/request/", 
+                headers: {},
+
+                data: JSON.stringify(
+                    {
+                        Method: "GET",
+                        Url: "https://replit.com/data/repls/@" + localStorage.replit_name + "/CODEX-INSTANCE"
+                    }
+                )
             }
         );
 
-        console.log(RequestData)
+        var Data = JSON.parse(JSON.parse(RequestData.data).Body)
+
+
+        console.log(Data)
+
+        if (Data.status != 404) {
+            document.getElementById("check").setAttribute("href", null)
+            document.getElementById("check").setAttribute("class", "next round disabled")
+            document.getElementById("check").innerText = "Found!"
+
+            document.getElementById("next-button").setAttribute("class", "next round enabled")
+
+        } else {
+            document.getElementById("check").innerHTML = "Check"
+        }
     }
 }
